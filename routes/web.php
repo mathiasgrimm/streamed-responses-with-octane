1572d91491920218822f74083403747011d06954
Mathias Grimm <mathiasgrimm@gmail.com>
2025-07-10 15:13:09 -0300

    wip

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/stream', function () {
+    return response()->stream(function (): void {
+        foreach (['developer', 'admin'] as $string) {
+            echo $string;
+            ob_flush();
+            flush();
+            sleep(2); // Simulate delay between chunks...
+        }
+    }, 200, ['X-Accel-Buffering' => 'no']);
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
